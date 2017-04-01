bd0041be0f2abcf0a3c638fc1a918809bee8ec1a
redhope <mrshanksster@gmail.com>
2017-03-31 21:35:11 -0500

    Learning cin, end of lecture 17.

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+	// Introduce the Game.
 	constexpr int WORD_LENGTH = 5;
 	cout << "Welcome to Bulls and Cows\n"; 
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of? \n";
+
+	//Ask the player for the guess.
+	string Player_Guess = "";
+	cin >> Player_Guess;
+
+	cout << "You entered: " << Player_Guess << "\n";
 	return 0;
 }
