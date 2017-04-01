1f923bd5387ee1578cb2c40fd38263e3189250d7
redhope <mrshanksster@gmail.com>
2017-03-31 21:57:12 -0500

    Abstraction of functions and prototyping. End of lecture 19.

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -2,16 +2,28 @@
 #include <string>
 using namespace std;
 
+string GetGuess();
+void PrintIntro();
 int main() {
-	// Introduce the Game.
-	constexpr int WORD_LENGTH = 5;
-	cout << "Welcome to Bulls and Cows\n"; 
-	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of? \n";
+	PrintIntro();
+	
+	string Player_Guess = GetGuess();
+	cout << "You entered: " << Player_Guess << endl;
+	return 0;
+}
 
-	//Ask the player for the guess.
+string GetGuess() {
 	string Player_Guess = "";
 	getline(cin, Player_Guess);
 
-	cout << "You entered: " << Player_Guess << endl;
-	return 0;
+	return Player_Guess;
+}
+
+void PrintIntro() {
+	// Introduce the Game.
+	constexpr int WORD_LENGTH = 5;
+	cout << "Welcome to Bulls and Cows\n";
+	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of? \n";
+
+	return;
 }
